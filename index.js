7fb5d4bbeed17d05226750347e53f7e7678263aa
Sunny17082 <91121970+Sunny17082@users.noreply.github.com>
2023-08-14 21:25:56 +0530

    Added notification

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ function opentab(){
     }
     event.currentTarget.classList.add("active");
 }
+
+let date = new Date().toLocaleDateString('en-us', {weekday:"long",month:"long", day:"numeric"});
+document.getElementById("date").innerHTML = date;
+
+
+function updateTime(){
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    if (minutes < 10){
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10){
+        seconds = "0" + seconds;
+    }
+    var t_str = hours + ":" + minutes + ":" +seconds;
+    document.getElementById('time').innerHTML = t_str;
+}
+setInterval(updateTime, 1000);
