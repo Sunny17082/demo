5bc8647dffb5ba8deb5b3d04a86b566a52fd9a42
Sunny17082 <91121970+Sunny17082@users.noreply.github.com>
2023-08-15 23:13:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const activeListItems = document.getElementsByClassName("list-item");
-function opentab(){
+ tabcontents=document.getElementsByClassName("tab-contents");
+function opentab(tabname){
     for(activeListItem of activeListItems){
         activeListItem.classList.remove("active");
     }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
     event.currentTarget.classList.add("active");
+    document.getElementById(tabname).classList.add("active-tab");
 }
 
 let date = new Date().toLocaleDateString('en-us', {weekday:"long",month:"long", day:"numeric"});
@@ -25,3 +30,37 @@ function updateTime(){
     document.getElementById('time').innerHTML = t_str;
 }
 setInterval(updateTime, 1000);
+
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let ctrl = document.getElementById("ctrl-icon");
+
+song.onloadedmetadata = function() {
+    progress.max=song.duration;
+    progress.val=song.currentTime;
+}
+
+function playpause() {
+    if(ctrl.classList.contains("fa-pause")) {
+        song.pause();
+        ctrl.classList.remove("fa-pause");
+        ctrl.classList.add("fa-play");
+    } else {
+        song.play();
+        ctrl.classList.add("fa-pause");
+        ctrl.classList.remove("fa-play");
+    }
+}
+
+if(song.play()) {
+    setInterval(() => {
+        progress.value = song.currentTime
+    },500);
+}
+
+progress.onchange = function() {
+    song.play();
+    song.currentTime=progress.value;
+    ctrl.classList.add("fa-pause");
+    ctrl.classList.remove("fa-play");
+}
