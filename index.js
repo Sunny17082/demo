82e4ff437f09882d529059ba48c456c4ed97648d
Sunny17082 <91121970+Sunny17082@users.noreply.github.com>
2023-08-19 14:29:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ setInterval(updateTime, 1000);
 let progress = document.getElementById("progress");
 let song = document.getElementById("song");
 let ctrl = document.getElementById("ctrl-icon");
+let curr_time = document.querySelector(".current-time");
+let total_duration = document.querySelector(".total-duration");
 
 song.onloadedmetadata = function() {
     progress.max=song.duration;
@@ -55,7 +57,23 @@ function playpause() {
 if(!song.pause()) {
     setInterval(() => {
         progress.value = song.currentTime
-    },500);
+        if(progress.value == song.max){
+            song.pause();
+        }
+
+        let currentMinutes = Math.floor(song.currentTime / 60);
+        let currentSeconds = Math.floor(song.currentTime - currentMinutes * 60);
+        let durationMinutes = Math.floor(song.duration / 60);
+        let durationSeconds = Math.floor(song.duration - durationMinutes * 60);
+
+        if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+        if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+        if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+        if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+        curr_time.textContent = currentMinutes + ":" + currentSeconds;
+        total_duration.textContent = durationMinutes + ":" + durationSeconds;
+    },1000);
 }
 
 progress.onchange = function() {
@@ -65,14 +83,18 @@ progress.onchange = function() {
     ctrl.classList.remove("fa-play");
 }
 
-const containers = document.getElementById("c1");
+const containerVideo = document.getElementById("cVideo");
 const myVideo = document.getElementById("myVideo");
 
 function playvid(videoname){
     myVideo.src = "/resources/"+videoname+".mp4";
     myVideo.play();
-    containers.classList.remove("no");
-    containers.classList.add("yes");
+    containerVideo.classList.remove("no");
+}
+
+function back(){
+    myVideo.pause();
+    containerVideo.classList.add("no");
 }
 
 function fun(e){
@@ -88,4 +110,30 @@ function thisVolume(volume_value){
     var song = document.getElementById("song");
     myvid.volume = volume_value / 100;
     song.volume = volume_value / 100;
+}
+
+var icon = document.getElementById("icon");
+
+icon.onclick = function() {
+    document.body.classList.toggle("light-theme");
+    if(document.body.classList.contains("light-theme")){
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+    } else {
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
+    }
+}
+
+const container = document.getElementById("container-main");
+const containerPowerOn = document.getElementById("cPowerOn");
+
+function powerOff(){
+    container.classList.add("no");
+    containerPowerOn.classList.remove("no");
+}
+
+function powerOn(){
+    containerPowerOn.classList.add("no");
+    container.classList.remove("no");
 }
