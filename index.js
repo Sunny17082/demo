b961e8302eb5786461692ce840c272f7ab9617a2
Sunny17082 <91121970+Sunny17082@users.noreply.github.com>
2023-08-24 21:42:50 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -211,3 +211,40 @@ function changeangle(){
         carcamera.src="resources/car.gif";
     }
 }
+
+let textarea = document.getElementById("textarea");
+let messageArea = document.querySelector(".message-area");
+
+textarea.addEventListener("keyup", (e) => {
+    if(e.key === "Enter") {
+        sendMessage(e.target.value);
+    }
+});
+
+function sendMessage(message) {
+    let msg = {
+        user: "You",
+        message: message.trim()
+    };
+    appendMessage(msg, "outgoing");
+    textarea.value="";
+    scrollToBottom();
+}
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let className = type;
+    mainDiv.classList.add(className, "message");
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p><pre>${msg.message}</pre></p>
+    `
+    
+    mainDiv.innerHTML = markup;
+    messageArea.appendChild(mainDiv);
+}
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight;
+}
